db4cb90e68bf89e9b16db086f36357c243462cf5
Drew Carmichael <drew.carmichael@okta.com>
2021-01-08 17:20:02 -0500

    added apply

--- lib/terraform_dir.py
+++ lib/terraform_dir.py
@@ -663,9 +663,9 @@ def apply_terraform_dir(
 # =============================================================================
 def apply_terraform_plan(
         terraform_dir: str,
-        terraform_plan_dir: Optional[str] = None,
         state_output_dir: Optional[str] = None,
         plan_file_path: Optional[str] = None,
+        terraform_plan_dir: Optional[str] = None,
         debug: bool = False) -> None:
     # check terraform dir
     if not terraform_dir:
